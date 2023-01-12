7adfaa2cb7cce115af3b8ce61c961a68654eae3e
Natalia Francisca <nathifrancisca@gmail.com>
2023-01-12 20:26:04 -0300

    feat: import components register user, group and address, this way is best to find the form elements

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -3,8 +3,12 @@ import { RegisterStyled, AlertMessageStyled } from "./Register.styled";
 import { useForm } from "react-hook-form";
 import { useEffect, useRef, useState } from "react";
 
-const Register = () => {
+import list_brazilian_states from "./list_brazilian_states.json";
+import RegisterUser from "./RegisterUser";
+import RegisterGroup from "./RegisterGroup";
+import RegisterAddress from "./RegisterAddress";
 
+const Register = () => {
 
     const ref_form = useRef();
 
@@ -19,6 +23,7 @@ const Register = () => {
         group:{
             name: '',
             sport: '',
+            address: {neighborhood: '', city: '', state: '', },
             instagram: '',
             schedule: [],
             group_type: '',
@@ -26,6 +31,7 @@ const Register = () => {
 
         }
     });
+
 
     const [showSucceedMessage, setShowSucceedMessage] = useState(false);
 
@@ -48,7 +54,7 @@ const Register = () => {
             setTimeout(() => {
                 setShowSucceedMessage(false);
                 console.log(showSucceedMessage)
-            }, 2000);
+            }, 8000);
         }
 
         console.log(showSucceedMessage)
@@ -59,230 +65,15 @@ const Register = () => {
             <h1>Preencha os dados abaixo para registrar seu grupo</h1>
 
             <form onSubmit={handleSubmit(onSubmit)} ref={ref_form}>
+                
+                <RegisterUser errors={errors} register={register} />
+                <RegisterGroup errors={errors} register={register} />
+                <RegisterAddress errors={errors} register={register} />
 
-                <AlertMessageStyled showMessage={showSucceedMessage}>
-                    <p>✅Cadastro realizado com sucesso</p>
-                </AlertMessageStyled>
-
-                <fieldset id="form-about-manager">
-                    <legend>Dados sobre o responsável</legend>
-
-                    <div className="group-input">
-                        <label className="label-input required-input" htmlFor="manager-name">Nome:</label>
-                        <input 
-                            type='text' 
-                            name="manager-name" 
-                            id="manager-name"
-                            placeholder="Nome do responsável"
-                            {...register("manager.name", { 
-                                required: "O campo nome deve ser preenchido", 
-                                minLength: {value: 2, message: "O nome deve conter no mínimo 2 caracteres"}, 
-                                maxLength: {value: 20, message: "O nome deve conter no máximo 20 caracteres"}
-                            })}
-                        />
-                        {errors.manager?.name && <p role="alert">{errors.manager.name?.message}</p>}
-                    </div>
-
-                    <div className="group-input">
-                        <label className="label-input required-input" htmlFor="manager-phone">Telefone:</label>
-                        <input 
-                            type="tel" 
-                            name="phone" 
-                            id="manager-phone"
-                            placeholder="Telefone para contato"
-                            {...register("manager.phone", { 
-                                required: "O campo de telefone deve ser preenchido",
-                                pattern: {value: /^(\d{10,11})/i, message: "Preencha corretamente o campo de telefone DD + número"}
-                            })}
-                        />
-                        {errors.manager?.phone && <p role="alert">{errors.manager.phone?.message}</p>}
-                    </div>       
-
-                    <div className="group-input">
-                        <label className="label-input required-input" htmlFor="manager-email">E-mail:</label>
-                        <input 
-                            type="email" 
-                            name="email" 
-                            id="manager-email"
-                            placeholder="E-mail para registro"
-                            {...register("manager.email", { 
-                                required: "O campo e-mail deve ser preenchido",
-                                pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: "Preencha corretamente o campo e-mail"} 
-                            })}
-                        />
-                        {errors.manager?.email && <p role="alert">{errors.manager.email?.message}</p>}
-                    </div>
-                   
-                    <div className="group-input">
-                        <label className="label-input required-input" htmlFor="manager-password">Senha:</label>
-                        <input 
-                            type="password" 
-                            name="password" 
-                            id="manager-password"
-                            placeholder="Senha para registro"
-                            {...register("manager.password", { 
-                                required: "O campo senha deve ser preenchido",
-                                minLength: {value: 6, message: "A senha deve conter no mínimo 6 caracteres"}, 
-                            })}
-                        />
-                        {errors.manager?.password && <p role="alert">{errors.manager.password?.message}</p>}
-                    </div>
-            
-                </fieldset>
-
-                <fieldset id="form-about-group">
-                    <legend>Dados sobre o grupo</legend>
-
-                    <div className="group-input">
-                        <label className="label-input required-input" htmlFor="group-name">Nome do Grupo:</label>
-                        <input 
-                            type="text" 
-                            name="group-name"
-                            id="group-name"
-                            placeholder="Digite o nome do grupo"
-                            {...register("group.name", {
-                                required: "O campo nome deve ser preenchido",
-                                minLength: {value: 2, message: "O campo deve ter no minímo 2 caracteres"}
-                            })}
-                        />
-                        {errors.group?.name && <p role="alert">{errors.group.name?.message}</p>}
-                    </div>
-
-                    <div className="group-input">
-                        <label className="label-input required-input" htmlFor="group-sport">Esporte:</label>
-                        <input 
-                            type="text"
-                            name="group-sport"
-                            id="group-sport"
-                            placeholder="Digite o esporte que o grupo prática"
-                            {...register("group.sport", {
-                                required: "O campo de esporte deve ser preenchido",
-                                minLength: {value: 2, message: "O campo deve ter no minímo 2 caracteres"}
-                            })}
-                        />
-                        {errors.group?.sport && <p role="alert">{errors.group.sport?.message}</p>}
-                    </div>
-
-                    <div className="group-input">
-                        <label className="label-input" htmlFor="group-instagram">Instagram:</label>
-                        <input 
-                            type="text" 
-                            name="group-instagram" 
-                            id="group-instagram"
-                            placeholder="Digite o instagram do grupo ou do responsável"
-                            {...register("group.instagram", { maxLength: 20})}
-                        />
-                    </div>  
-
-                    <div className="group-input">
-                        <label className="label-input required-input"> Hórario: </label>
-                            <label>
-                                <input 
-                                    type="checkbox" 
-                                    name="group-date-time" 
-                                    value="Matutino" 
-                                    {...register("group.schedule", {required: 'Escolha um grupo de horário'})}
-                                />
-                                Matutino (06:00 - 12:00)
-                            </label>
-
-                            <label>
-                                <input 
-                                    type="checkbox" 
-                                    name="group-date-time" 
-                                    value="Vespertino" 
-                                    {...register("group.schedule", {required: 'Escolha um grupo de horário'})}
-                                />
-                                Vespertino (12:00 - 18:00)
-                            </label>
-
-                            <label>
-                                <input 
-                                    type="checkbox" 
-                                    name="group-date-time" 
-                                    value="Noturno" 
-                                    {...register("group.schedule", {required: 'Escolha um grupo de horário'})}
-                                />
-                                Noturno (18:00 - 00:00)
-                            </label>
-
-                            {errors.group?.schedule && <p role="alert">{errors.group.schedule?.message}</p>}     
-                    </div>
-                    
-                    <div className="group-input">
-                        <label className="label-input required-input"> O grupo se encaixa em quais opções: </label>
-                            
-                            <label>
-                                <input 
-                                    type="checkbox" 
-                                    name="group-type"  
-                                    value="Recreativo" 
-                                    {...register("group.group_type", {required: 'Escolha um grupo de horário'})}
-                                />
-                                Recreativo
-                            </label>
-
-                            <label>
-                                <input 
-                                    type="checkbox" 
-                                    name="group-type"  
-                                    value="Amador" 
-                                    {...register("group.group_type", {required: 'Escolha um grupo de horário'})}
-                                />
-                                Amador
-                            </label>
-
-                            <label>
-                                <input 
-                                    type="checkbox" 
-                                    name="group-type"  
-                                    value="Profissional" 
-                                    {...register("group.group_type", {required: 'Escolha um grupo de horário'})}
-                                />
-                                Profissional
-                            </label>
-                          
-                            {errors.group?.group_type && <p role="alert">{errors.group.group_type?.message}</p>}     
-                    </div>
-
-                    <div className="group-input">
-                        <label className="label-input required-input">Tipo de pagamento para acesso e participação:</label>
-
-                            <label>
-                                <input 
-                                    type="radio" 
-                                    name="group-price-rules" 
-                                    value="free" 
-                                    {...register("group.price", {required: 'Escolha uma das opções de pagamento'})}
-                                />
-                                Gratuito
-                            </label>
-
-                            <label>
-                                <input 
-                                    type='radio' 
-                                    name="group-price-rules" 
-                                    value="month-payment" 
-                                    {...register("group.price", {required: 'Escolha uma das opções de pagamento'})}
-                                />
-                                Pagamento mensal
-                            </label>
-
-                            <label>
-                                <input 
-                                    type='radio' 
-                                    name="group-price-rules" 
-                                    value="game-payment" 
-                                    {...register("group.price", {required: 'Escolha uma das opções de pagamento'})}
-                                />
-                                Pagamento por dia de jogo
-                            </label>
-
-                            {errors.group?.price && <p role="alert">{errors.group.price?.message}</p>}     
-                    </div>
-                </fieldset>
-
+                
                 <button type="submit">Registrar</button>
+
+                <button onClick={() => setShowSucceedMessage(true)}>TESTE</button>
 
             </form>
         </RegisterStyled>
