dde42e97baa127cd02e1f8299ab2ce0af38550d7
Natalia Francisca <nathifrancisca@gmail.com>
2023-01-13 19:46:44 -0300

    feat: change all the form to the new version

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,79 +1,86 @@
-import { RegisterStyled, AlertMessageStyled } from "./Register.styled";
+import { RegisterStyled } from "./Register.styled";
 
 import { useForm } from "react-hook-form";
-import { useEffect, useRef, useState } from "react";
-
-import list_brazilian_states from "./list_brazilian_states.json";
-import RegisterUser from "./RegisterUser";
-import RegisterGroup from "./RegisterGroup";
-import RegisterAddress from "./RegisterAddress";
+import { useRef } from "react";
 
 const Register = () => {
 
     const ref_form = useRef();
 
-    const { register, reset,  formState: { errors }, handleSubmit } = useForm({
-        manager:{
+    const { register, formState: { errors }, handleSubmit } = useForm({
             name: '',
             email: '',
-            phone: '',
             password: ''
-        },
-
-        group:{
-            name: '',
-            sport: '',
-            address: {neighborhood: '', city: '', state: '', },
-            instagram: '',
-            schedule: [],
-            group_type: '',
-            price: ''
-
-        }
     });
-
-
-    const [showSucceedMessage, setShowSucceedMessage] = useState(false);
-
     
     const onSubmit = data => {
 
         const objErrorIsEmpty = JSON.stringify(errors) === '{}';
 
-        console.log(data);
-
         if(objErrorIsEmpty){
+            console.log(data);
             ref_form.current.reset();
-            setShowSucceedMessage(true)
         }
     }
     
-
-    useEffect(() => {
-        if(showSucceedMessage){
-            setTimeout(() => {
-                setShowSucceedMessage(false);
-                console.log(showSucceedMessage)
-            }, 8000);
-        }
-
-        console.log(showSucceedMessage)
-    },[showSucceedMessage])
-
     return(
         <RegisterStyled>
-            <h1>Preencha os dados abaixo para registrar seu grupo</h1>
 
             <form onSubmit={handleSubmit(onSubmit)} ref={ref_form}>
-                
-                <RegisterUser errors={errors} register={register} />
-                <RegisterGroup errors={errors} register={register} />
-                <RegisterAddress errors={errors} register={register} />
+                <h1>Registro</h1>
 
-                
-                <button type="submit">Registrar</button>
+                <div className="group-input">
+                    <label className="label-input required-input" htmlFor="name">Nome:</label>
+                    <input 
+                        type='text' 
+                        name="name" 
+                        id="name"
+                        placeholder="Nome do responsável"
+                        {...register("name", { 
+                            required: "O campo nome deve ser preenchido", 
+                            minLength: {value: 2, message: "O nome deve conter no mínimo 2 caracteres"}, 
+                            maxLength: {value: 20, message: "O nome deve conter no máximo 20 caracteres"}
+                        })}
+                    />
+                    {errors.name && <p role="alert">{errors.name?.message}</p>}
+                </div>
 
-                <button onClick={() => setShowSucceedMessage(true)}>TESTE</button>
+                <div className="group-input">
+                    <label className="label-input required-input" htmlFor="manager-email">E-mail:</label>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email"
+                        placeholder="E-mail para registro"
+                        {...register("email", { 
+                            required: "O campo e-mail deve ser preenchido",
+                            pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: "Preencha corretamente o campo e-mail"} 
+                        })}
+                    />
+                    {errors.email && <p role="alert">{errors.email?.message}</p>}
+                </div>
+
+                <div className="group-input">
+                    <label className="label-input required-input" htmlFor="password">Senha:</label>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        id="password"
+                        placeholder="Senha para registro"
+                        {...register("password", { 
+                            required: "O campo senha deve ser preenchido",
+                            minLength: {value: 6, message: "A senha deve conter no mínimo 6 caracteres"}, 
+                        })}
+                    />
+                    {errors?.password && <p role="alert">{errors.password?.message}</p>}
+                </div>
+
+                <button type="submit" id='btn-register'>Registrar</button>
+
+                <button id="btn-register-google">
+                    <img src="assets/icon/icon-google.svg" />
+                    Registrar com Google
+                </button>
 
             </form>
         </RegisterStyled>
