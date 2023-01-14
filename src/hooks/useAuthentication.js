42dfd0e2a3de8ebf8dae55e2b8d35858b3295673
Natalia Francisca <nathifrancisca@gmail.com>
2023-01-13 21:31:14 -0300

    feat: add error messages to function create user; add funtion create user with google

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -4,7 +4,9 @@ import {
     createUserWithEmailAndPassword, 
     signOut, 
     signInWithEmailAndPassword,
-    updateProfile
+    updateProfile,
+    GoogleAuthProvider,
+    signInWithPopup
  } from "firebase/auth";
 
 import { useState, useEffect } from "react"
@@ -12,6 +14,8 @@ import { useState, useEffect } from "react"
 export const useAuthentication = () => {
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(null);
+
+    const provider = new GoogleAuthProvider();
 
     const createUser = async(data) => {
 
@@ -28,9 +32,45 @@ export const useAuthentication = () => {
 
             return user;
         }catch(error){
-            console.log(error.message);
+            let systemErrorMessage;
+
+            if(error.message.includes("Password")){
+                systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres."
+            }else if(error.message.includes("email-already")){
+                systemErrorMessage = "Esse e-mail já está sendo utilizado.";
+            }else{
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde"
+            }
+
+            setError(systemErrorMessage);
+        }
+    }
+
+    const createUserGoogle = async() => {
+        provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+        try{
+            const result = await signInWithPopup(auth, provider)
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+
+            const user = result.user;
+
+            return user;
+        }catch(error){
+            const errorCode = error.code;
+            const errorMessage = error.message;
+
+            const email = error.customData.email;
+            const credential = GoogleAuthProvider.credentialFromError(error);
+
+            console.log(errorCode);
+            console.log(errorMessage);
+            console.log(email);
+            console.log(credential);
 
         }
+     
     }
 
     const logout = () => {
@@ -45,12 +85,14 @@ export const useAuthentication = () => {
         }
     }
 
+
     return {
         auth,
         createUser,
         error,
         logout,
-        login
+        login,
+        createUserGoogle
     }
 
 
