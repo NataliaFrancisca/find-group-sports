10a177e3963609cb9eb7be9092926d73d782313b
Natalia Francisca <nathifrancisca@gmail.com>
2023-01-17 22:12:44 -0300

    feat: change nav links to show only when the user is login or logout; create the button to logout

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,36 @@
+import { NavLink } from "react-router-dom"
 
-import {NavLink} from "react-router-dom"
+import { useAuthValue  } from "../../context/AuthContext";
+import { useAuthentication } from "../../hooks/useAuthentication";
 
 import "./NavBarStyle.css";
 
 const NavBar = () => {
+
+    const { logout } = useAuthentication();
+    const { user } = useAuthValue();
+
     return(
         <menu>
             <nav>
                 <NavLink to="/" end>Home</NavLink>
                 <ul>
-                    <NavLink to="/feed">Grupos</NavLink>
-                    <NavLink to="/register">Registro</NavLink>
-                    <NavLink to="/login">Login</NavLink>
+                    <NavLink to="/groups">Grupos</NavLink>
+
+                    {!user && (
+                        <>
+                            <NavLink to="/register">Registro</NavLink>
+                            <NavLink to="/login">Login</NavLink>
+                        </>
+                    )}
+
+                    {user && (
+                        <>
+                            <NavLink to="/dashboard">Dashboard</NavLink>
+                        </>
+                    )}
+
+                    {user && <button onClick={logout}>Sair</button>}
                 </ul>
             </nav>
         </menu>
