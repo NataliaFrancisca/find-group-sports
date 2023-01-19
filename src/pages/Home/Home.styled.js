835ce8e05eedda3e4a74c4b7853996959880eaa0
Natalia Francisca <nathifrancisca@gmail.com>
2023-01-18 22:45:02 -0300

    test: testing the new style

--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -1,29 +1,97 @@
 import styled from "styled-components";
 
 export const HomeStyled = styled.header`
-    display: flex;
+    display: grid;
+    grid-template-columns: 50% 50%;
     height: auto;
     justify-content: space-between;
-    min-height: 90vh;
+    margin: 10px 20px;
+    min-height: 85vh;
+    height: auto;
+    position: relative;
+    border: solid var(--c-black) 4px;
+    
+
+
+    /* display: flex;
+    height: auto;
+    justify-content: space-between;
+    min-height: 85vh;
+    height: auto;
+    position: relative;
+    border: solid var(--c-black) 4px;
+    margin: 10px 20px; */
+
+    &:before{
+        content: "";
+        border: solid var(--c-orange) 4px;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 4px;
+        right: 10px;
+    }
+
+    @media (width:320px) { }
+    @media (width:480px) { }
+
+    @media screen and (max-width: 600px) { 
+        grid-template-columns: 100%;
+        gap: 30px;
+        padding: 20px 0;
+    }
+
+    @media screen and (max-width: 801px) {
+
+    }
 
     section.container-text{  
         display: flex;
         flex-direction: column;
         justify-content: center;
-        padding: 10px 40px;
-        width: 50%;
+        padding: 0 20px 0 20px;
+        width: 100%;
 
         & h1.title{
-            font-size: 5em;
+            font-size: 8em;
             font-weight: 600;
-            line-height: 8rem;
-            letter-spacing: 6px;
-            text-transform: capitalize;
+            line-height: 6rem;
+            letter-spacing: -8px;
+            text-transform: uppercase;
         }
 
         & p.subtitle{
             font-size: 1.8em;
             margin-top: 22px;
+        }
+
+        & button.btn-navigate-page{
+            align-self: flex-start;
+            background-color: var(--c-green);
+            color: var(--c-black);
+            display: flex;
+            font-size: 1.2em;
+            font-weight: 600;
+            letter-spacing: 2px;
+            margin-top: 60px;
+            padding: 24px 16px;
+            z-index: 1;
+
+            &:hover{
+                background-color: var(--c-black);
+                color: var(--c-white);
+            }
+        }
+
+        @media screen and (max-width:600px) {
+            & h1.title{
+                font-size: 7em;
+            }
+
+            & button.btn-navigate-page{
+                font-size: 1.6em;
+                padding: 36px 20px;
+            }
         }
     }
 
@@ -31,7 +99,7 @@ export const HomeStyled = styled.header`
     section.container-images{
         display: flex;
         position: relative;
-        width: 50%;
+        width: 100%;
 
         & .first-group{
             display: flex;
@@ -43,10 +111,15 @@ export const HomeStyled = styled.header`
         & .second-group{
             margin-top: 40px;
         }
+
+        
+        @media screen and (max-width: 600px) { 
+            display: none;
+        }
     }
 
     #img-circle{
-        border: solid var(--c-orange) 2px;
+        background: var(--c-orange);
         border-radius: 120px 120px;
         height: 360px;
         position: relative;
@@ -82,7 +155,7 @@ export const HomeStyled = styled.header`
     }
 
     #img-square{
-        border: solid var(--c-purple) 4px;
+        background: var(--c-purple);
         height: 300px;
         margin-left: 40px;
         margin-top: 60px;
@@ -96,25 +169,6 @@ export const HomeStyled = styled.header`
             position: absolute;
             top: -20px;
             width: 340px; 
-        }
-    }
-
-    /* BUTTON  */
-    .btn-navigate-page{
-        align-self: flex-end;
-        background-color: #489446;
-        color: #e7e8de;
-        display: flex;
-        font-size: 1.2em;
-        font-weight: 400;
-        justify-self: flex-end;
-        letter-spacing: 2px;
-        margin-top: 60px;
-        padding: 24px 16px;
-        text-shadow: 1px 1px var(--c-black);
-
-        &:hover{
-            background-color: var(--c-black);
         }
     }
 
