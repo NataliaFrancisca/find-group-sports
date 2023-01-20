42ebcb7b0f8b1f09607ab3170732588b6b93d1f8
Natalia Francisca <nathifrancisca@gmail.com>
2023-01-19 22:27:08 -0300

    feat: remove the columns from the container-iamges and put the button inside the container-text

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,29 +9,26 @@ const Home = () => {
     return(
         <HomeStyled>
             <section className="container-text">
-                <h1 className="title">Bora <br /> Jogar?</h1>
+                <h1 className="title">Bora Jogar?</h1>
                 <p className="subtitle">Encontre um grupo para você praticar esportes.</p>
-
                 <button className="btn-navigate-page" onClick={() => navigate("/feed")}>Find your group now</button>
             </section>
 
-            {/* <section className="container-images">
-                <div className="first-group">
-                    <div id="img-circle">
-                        <img src="assets/images/sport-tenis.jpg" alt="raquete de tenis" />
-                    </div>
+            <section className="container-images">
+           
+                <div id="img-circle">
+                    <img src="assets/images/sport-tenis.jpg" alt="raquete de tenis" />
                 </div>
 
-                <div className="second-group">
-                    <div id="img-semi-cirle">
-                        <img src="assets/images/sport-basketball.jpg" alt="cesta de basquete"/>
-                    </div>
-                    
-                    <div id="img-square">
-                        <img src="assets/images/sport-run.jpg" alt="pista de corrida" />
-                    </div>
+                <div id="img-semi-cirle">
+                    <img src="assets/images/sport-basketball.jpg" alt="cesta de basquete"/>
                 </div>
-            </section> */}
+
+                <div id="img-square">
+                    <img src="assets/images/sport-run.jpg" alt="pista de corrida" />
+                </div>
+                
+            </section>
         </HomeStyled>
     )
 }
