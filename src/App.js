8d35168f6f92978db33d4fdb8c47be185fb43456
Natalia Francisca <nathifrancisca@gmail.com>
2023-01-30 22:08:58 -0300

    style: add style rules to app component and add the background home filter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Navigate, useLocation} from "react-router-dom"
 
 import Home from "./pages/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import DashBoard from "./pages/Dashboard/Dashboard";
+
+import { AppStyled, BackgroundFilter } from "./styles/Global.styled";
 
 import CreateGroup from "./pages/CreateGroup/CreateGroup";
 import { AuthProvider } from "./context/AuthContext";
@@ -18,6 +20,8 @@ function App() {
   const [user, setUser] = useState();
   const { auth } = useAuthentication();
 
+  const location = useLocation();
+
   const loadingUser = user === undefined;
 
   useEffect(() => {
@@ -27,9 +31,9 @@ function App() {
   },[auth])
 
   return (
-    <div className="App">
+    <AppStyled>
+      {location.pathname == "/" && <BackgroundFilter />}
       <AuthProvider value={{ user }}>
-        <BrowserRouter>
           <NavBar />
             <Routes>
               <Route path="/" element={<Home />} />
@@ -54,9 +58,8 @@ function App() {
                 element={!user ? <Login /> : <Navigate to="/" />}  
               />
             </Routes>
-        </BrowserRouter>
       </AuthProvider>
-    </div>
+    </AppStyled>
   );
 }
 
