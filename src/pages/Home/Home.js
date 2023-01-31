5879d54f3f44070dfe609c2f596b93569ccd9717
Natalia Francisca <nathifrancisca@gmail.com>
2023-01-30 22:09:54 -0300

    feat: change the home style and remove the images

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import { HomeStyled, GroupImagesStyled} from "./Home.styled";
+import { HomeStyled} from "./Home.styled";
 
 import { useNavigate } from "react-router-dom";
 
@@ -9,24 +9,10 @@ const Home = () => {
     return(
         <HomeStyled>
             <section className="container-text">
-                <h1 className="title">Bora Jogar?</h1>
+                <h1 className="title">Bora <br/> Jogar?</h1>
                 <p className="subtitle">Encontre um grupo para você praticar esportes.</p>
                 <button className="btn-navigate-page" onClick={() => navigate("/feed")}>Find your group now</button>
             </section>
-
-            <GroupImagesStyled>
-                <div className="div-image-home circle">
-                    <img src="assets/images/sport-tenis.jpg" alt="raquete de tenis" />
-                </div>
-
-                <div className="div-image-home semi-circle">
-                    <img src="assets/images/sport-basketball.jpg" alt="cesta de basquete"/>
-                </div>
-
-                <div className="div-image-home square">
-                    <img src="assets/images/sport-run.jpg" alt="pista de corrida" />
-                </div> 
-            </GroupImagesStyled>
         </HomeStyled>
     )
 }
