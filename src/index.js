03f038530805e866b1e6a2ca9661514a17cb6f59
Natalia Francisca <nathifrancisca@gmail.com>
2023-02-05 22:10:55 -0300

    feat: change the styled-component file to a CSS file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { GlobalStyle } from './styles/Global.styled';
+
+import "./index.css";
+
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
     <BrowserRouter>
       <App />
     </BrowserRouter>
